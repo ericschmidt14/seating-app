baec9b8a79ef299ebda23e19579ca6ce61da0854
E R I C S C H M I D T <info@ericschmidt.de>
2024-11-07 11:18:35 +0100

    fix

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,12 +1,13 @@
 import { FCN_WEB_API } from "@/app/constants";
 
 export async function GET() {
-  const res = await fetch(`${FCN_WEB_API}/seat/save`, {
+  const res = await fetch(FCN_WEB_API, {
     method: "GET",
     headers: {
       Accept: "*/*",
       "Content-Type": "application/json; charset=UTF-8",
     },
+    cache: "no-store",
   });
   const data = await res.json();
   return Response.json(data);
