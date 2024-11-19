58a40c190b4a42fd832bca49e311e19b1c475676
E R I C S C H M I D T <info@ericschmidt.de>
2024-11-19 14:38:14 +0100

    tweak pitch

--- src/app/components/pitch.tsx
+++ src/app/components/pitch.tsx
@@ -4,12 +4,16 @@ import { useSeating } from "../context/seatingContext";
 export default function Pitch() {
   const { lounge } = useSeating();
 
-  const width = lounge === 1 ? 1600 : lounge === 2 ? 2900 : 2000;
+  const width: Record<number, number> = {
+    1: 1600,
+    2: 2900,
+    3: 2000,
+  };
 
   return (
     <div
-      className="relative flex justify-center p-2 rounded bg-white/15"
-      style={{ width: `${width}px` }}
+      className="relative flex justify-center p-4 rounded bg-black/15"
+      style={{ width: `${width[lounge]}px` }}
     >
       Spielfeld
     </div>
