0df3d7e2427e0f89594b658b0d48d93e7e976d21
E R I C S C H M I D T <info@ericschmidt.de>
2024-11-27 15:47:10 +0100

    fix serviceworker

--- next.config.js
+++ next.config.js
@@ -1,17 +1,39 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-const runtimeCaching = require("next-pwa/cache");
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: !isProd,
+  disable: process.env.NODE_ENV !== "production",
   register: true,
   skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/, /app-build-manifest\.json$/],
   fallbacks: {
     document: "/offline.html",
   },
-  runtimeCaching,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/seating\.fcn\.de\/_next\/data\/.+\.json$/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "next-data",
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 86400,
+        },
+      },
+    },
+    {
+      urlPattern: /\/.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "pages",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 86400,
+        },
+      },
+    },
+  ],
 });
 
 module.exports = withPWA({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 import App from "./components/app";
 import { SeatingProvider } from "./context/seatingContext";
 
 export default function Home() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js", { scope: "/" })
+          .then((registration) => {
+            console.log(
+              "Service Worker registered with scope:",
+              registration.scope
+            );
+          })
+          .catch((error) => {
+            console.error("Service Worker registration failed:", error);
+          });
+      });
+    }
+  }, []);
+
   return (
     <SessionProvider>
       <SeatingProvider>
